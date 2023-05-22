9761f10942a8187670b9f53a9f2e605b7370576f
“Huma <humabhutti>
2023-05-23 04:10:48 +0500

    20 assi

--- Assignments/Assignment-9-10/app.js
+++ Assignments/Assignment-9-10/app.js
@@ -1,7 +1,26 @@
 // 1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+// var userInput = prompt("City Name...")
+//  if(userInput == 'Karachi' || userInput == 'karachi') {
+//     alert("Welcome to city of lights");
+// }
+
 // 2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt("Enter Your Gender")
+// if (gender == 'male' || gender == 'Male') {
+//     alert("Good Morning Sir.")
+// }
+// else if (gender == 'female' || gender == 'Female') {
+//     alert("Good Morning Ma’am.")
+// }
+
 // 3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+
+// var userInput =
+
 // 4. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”
+
 //     5. Run this script, & check whether alert message would be displayed or not. Record the outputs.
 //     a. var a = 4;
 //     if (++a === 5){
@@ -36,18 +55,17 @@
 // if (false){
 // alert("False");
 // }
-// 
+//
 // f. if("car" < "cat"){
 //     alert("car is smaller than cat");
 //     }
-//     6. Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:
+// 6. Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:
+
 // 7. Guess game:
 // Store a secret number (ranging from 1 to 10) in a variable.
 // Prompt user to guess the secret number.
-// a. If user guesses the same number, show “Bingo! Correct
-// answer”.
-// b. If the guessed number +1 is the secret number, show
-// “Close enough to the correct answer”.
+// a. If user guesses the same number, show “Bingo! Correct answer”.
+// b. If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
 
 // var secretNumber = 1;
 // var userInput = +prompt("Guess the number between 1 to 10")
@@ -62,21 +80,36 @@
 
 // 8. Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
 
-var number = 21; 
-if( number % 3 == 0) {
-alert(number)
-}
+// var number = 21;
+// if( number % 3 == 0) {
+// alert(number)
+// }
 // {The number is divisible by three. Do something... }
 
 // 9. Write a program that checks whether the given input is an even number or an odd number.
+
+// var userInput = ("Enter a Number")
+// if
+
 // 10. Write a program that takes temperature as input and shows a message based on following criteria
 // a. T > 40 then “It is too hot outside.”
 // b. T > 30 then “The Weather today is Normal.”
 // c. T > 20 then “Today’s Weather is cool.”
 // d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+var temperature = prompt("Enter Temperature");
+if ("t > 40") {
+  alert("It is too hot outside.");
+} else if ("t > 30") {
+  alert("The Weather today is Normal.");
+} else if ("t > 20") {
+  alert("Today’s Weather is cool.");
+} else if ("t > 10") {
+  alert("OMG! Today’s weather is so Cool.");
+}
+
 // 11. Write a program to create a calculator for +,-,*, / & % using if statements. Take the following input:
 // a. First number
 // b. Second number
 // c. Operation (+, -, *, /, %)
 // Compute & show the calculated result to user.
-    
